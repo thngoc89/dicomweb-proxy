0ad0c96aa5e5c3ce37c71658c5bb32aeead7f412
Michael Knopke <knopkem@gmail.com>
2020-06-10 15:08:19 +0200

    first working version

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 const logger = utils.getLogger();
 
 // unprotected middleware passing
-let middle = function middle(req, res, next) {
+const middle = function middle(req, res, next) {
   next();
 };
 /*
@@ -46,11 +46,11 @@ shell.mkdir("-p", "./data");
 app.use(express.static("public"));
 
 // prevents nodejs from exiting
-process.on("uncaughtException", err => {
+process.on("uncaughtException", (err) => {
   logger.info("uncaught exception received:");
-  logger.info("------------------------------------------")
+  logger.info("------------------------------------------");
   logger.error(err.stack);
-  logger.info("------------------------------------------")
+  logger.info("------------------------------------------");
 });
 
 //------------------------------------------------------------------
@@ -125,7 +125,7 @@ app.get(
 
     const json = await utils.doFind("IMAGE", query, tags);
     // fetch series but wait for first image only
-    const waitPromise = await utils.waitOrFetchData(
+    await utils.waitOrFetchData(
       studyInstanceUid,
       seriesInstanceUid,
       true
@@ -137,11 +137,9 @@ app.get(
       return;
     }
 
-    const sopInstanceUid = json[0]["00080018"]["Value"][0];
+    const sopInstanceUid = json[0]["00080018"].Value[0];
     const storagePath = config.get("storagePath");
-    console.log(storagePath, studyInstanceUid, sopInstanceUid);
-    const pathname =
-      path.join(storagePath, studyInstanceUid, sopInstanceUid) + ".dcm";
+    const pathname = path.join(storagePath, studyInstanceUid, `${sopInstanceUid}.dcm`);
 
     fs.readFile(pathname, (err, data) => {
       if (err) {
@@ -151,105 +149,97 @@ app.get(
       }
       const dataset = dicomParser.parseDicom(data);
 
-      // parse additional needed attributes
-      const bitsAllocated = dataset.uint16("x00280100");
-      const bitsStored = dataset.uint16("x00280101");
-      const highBit = dataset.uint16("x00280102");
-      const rows = dataset.uint16("x00280010");
-      const cols = dataset.uint16("x00280011");
-      const pixelSpacing = dataset.string("x00280030");
-      const modality = dataset.string("x00080060");
-
       // all tags
       /*
       "0008193E" "LO"
       "0020000D" "UI"
       "0020000E" "UI"
       
-      "00080005" "CS"
-      "00080008" "CS"
-      "00080016" "UI"
-      "00080018" "UI"
-      "00080020" "DA"
-      "00080021" "DA"
-      "00080022" "DA"
-      "00080023" "DA"
-      "00080030" "TM"
-      "00080031" "TM"
-      "00080032" "TM"
-      "00080033" "TM"
-      "00080050" "SH"
-      "00080060" "CS"
-      "00080070" "LO"
-      "00080080" "LO"
-      "00080090" "PN"
-      "00081010" "SH"
-      "00081030" "LO"
-      "00081032" "SQ"
-      "00081040" "LO"
-      "00081070" "PN"
-      "00081090" "LO"
-      "00081110" "SQ"
+      "00080005" "CS" "Specific Character Set"
+      "00080008" "CS" "Image Type"
+      "00080016" "UI" "SOP Class UID"
+      "00080018" "UI" "SOP Instance UID"
+      "00080020" "DA" "Study Date"
+      "00080021" "DA" "Series Date"
+      "00080022" "DA" "Acquisition Date"
+      "00080023" "DA" "Content Date"
+      "00080030" "TM" "Study Time"
+      "00080031" "TM" "Series Time"
+      "00080032" "TM" "Acq. Time"
+      "00080033" "TM" "Content Time"
+      "00080050" "SH" "Accession Number"
+      "00080060" "CS" "Modality"
+      "00080070" "LO" "Manufacturer"
+      "00080080" "LO" "Institution Name"
+      "00080090" "PN" "Referring Physician's Name"
+      "00081010" "SH" "Station Name"
+      "00081030" "LO" "Study Description"
+      "00081032" "SQ" "Procedure Code Sequence"
+      "00081040" "LO" "Institutional Department Name"
+      "00081070" "PN" "Operators Name"
+      "00081090" "LO" "Manufacturer's Model Name"
+      "00081110" "SQ" "Referenced Study Sequence"
       
-      "00100010" "PN"
-      "00100020" "LO"
-      "00100021" "LO"
-      "00100030" "DA"
-      "00100040" "CS"
-      "00101000" "LO"
-      "00101010" "AS"
-      "00101020" "DS"
-      "00101030" "DS"
-      "00104000" "LT"
+      "00100010" "PN" "Patient's Name"
+      "00100020" "LO" "Patient ID"
+      "00100021" "LO" "Issuer of Patient ID"
+      "00100030" "DA" "Patient's Birth Date"
+      "00100040" "CS" "Patient Sex"
+      "00101000" "LO" "?"
+      "00101010" "AS" "Patient's Age"
+      "00101020" "DS" "Patient's Size"
+      "00101030" "DS" "Patient's Weight"
+      "00104000" "LT" "Patient Comments"
 
-      "00180015" "CS"
-      "00180022" "CS"
-      "00180050" "DS"
-      "00180060" "DS"
-      "00180090" "DS"
-      "00181000" "LO"
-      "00181020" "LO"
-      "00181030" "LO"
-      "00181100" "DS"
-      "00181120" "DS"
-      "00181130" "DS"
-      "00181140" "CS"
-      "00181150" "IS"
-      "00181151" "IS"
-      "00181152" "IS"
-      "00181160" "SH"
-      "00181170" "IS"
-      "00181190" "DS"
-      "00181210" "SH"
-      "00185100" "CS"
+      "00180015" "CS" "Body Part Examined"
+      "00180022" "CS" "Scan Options"
+      "00180050" "DS" "Slice Thickness"
+      "00180060" "DS" "KVP"
+      "00180090" "DS" "Data Collection Diameter"
+      "00181000" "LO" "Device Serial Number"
+      "00181020" "LO" "Software Version"
+      "00181030" "LO" "Protocol Name"
+      "00181100" "DS" "Reconstruction Diameter"
+      "00181120" "DS" "Gantry/Detector Tilt"
+      "00181130" "DS" "Table Height"
+      "00181140" "CS" "Rotation Direction"
+      "00181150" "IS" "Exposure Time"
+      "00181151" "IS" "X-Ray Tube"
+      "00181152" "IS" "Exposure Attribute"
+      "00181160" "SH" "Filter Type"
+      "00181170" "IS" "Generator Power"
+      "00181190" "DS" "Focal Spots"
+      "00181210" "SH" "Convolution Kernel"
+      "00185100" "CS" "Patient Position"
 
-      "00200010" "SH"
-      "00200011" "IS"
-      "00200012" "IS"
-      "00200013" "IS"
-      "00200020" "CS"
-      "00200032" "DS"    
-      "00200037" "DS"
-      "00200052" "UI"
-      "00200040" "LO"
-      "00200041" "DS"
+      "00200010" "SH" "Study ID"
+      "00200011" "IS" "Series Number"
+      "00200012" "IS" "Acquisition Number"
+      "00200013" "IS" "Instance Number"
+      "00200020" "CS" "Patient Orientation"
+      "00200032" "DS" "Image Postion"
+      "00200037" "DS" "Image Orientation"
+      "00200052" "UI" "Frame of Reference UID"
+      "00201040" "LO" "Position Reference Indicator"
+      "00201041" "DS" "Slice Location"
 
-      "00280002" "US"
-      "00280004" "CS"
-      "00280010" "US"
-      "00280011" "US"
-      "00280030" "DS"
-      "00280100" "US"
-      "00280101" "US"
-      "00280102" "US"
-      "00280103" "US"
-      "00281050" "DS"
-      "00281051" "DS"
-      "00281052" "DS"
-      "00281053" "DS"
+      "00280002" "US" "Samples per Pixel"
+      "00280004" "CS" "Photometric Interpretation"
+      "00280010" "US" "Rows"
+      "00280011" "US" "Columns"
+      "00280030" "DS" "Pixel Spacing"
+      "00280100" "US" "Bits Allocated"
+      "00280101" "US" "Bits Stored"
+      "00280102" "US" "High Bit"
+      "00280103" "US" "Pixel Representation"
+      "00281050" "DS" "Window Center"
+      "00281051" "DS" "Window Width"
+      "00281052" "DS" "Rescale Intercept"
+      "00281053" "DS" "Rescale Slope"
  
-      "00321033" "LO"
-      "00400002" "DA"
+      // modality 
+      "00321033" "LO" "Requesting Service"
+      "00400002" "DA" ""
       "00400003" "TM"
       "00400004" "DA"
       "00400005" "TM"
@@ -257,17 +247,46 @@ app.get(
       "00400245" "TM"
       "00400253" "SH"
       "00400260" "SQ"
-*/
+     */
+
+      // parse additional needed attributes
+      const bitsAllocated = dataset.uint16("x00280100");
+      const bitsStored = dataset.uint16("x00280101");
+      const highBit = dataset.uint16("x00280102");
+      const rows = dataset.uint16("x00280010");
+      const cols = dataset.uint16("x00280011");
+      const pixelSpacing = dataset.string("x00280030");
+      const arr = pixelSpacing ? pixelSpacing.split("\\") : ["1", "1"];
+      const px = parseFloat(arr[0]);
+      const py = parseFloat(arr[1]);
+      const modality = dataset.string("x00080060");
+      const samplesPerPixel = dataset.uint16("x00280002");
+      const photometricInterpretation = dataset.string("x00280004");
+      const pixelRepresentation = dataset.uint16("x00280103");
+      const windowCenter = dataset.string("x00281050");
+      const wc = windowCenter ? parseFloat(windowCenter.split("\\")[0]) : 40;
+      const windowWidth = dataset.string("x00281051");
+      const ww = windowWidth ? parseFloat(windowWidth.split("\\")[0]) : 80;
+      const rescaleIntercept = parseFloat(dataset.string("x00281052"));
+      const rescaleSlope = parseFloat(dataset.string("x00281053"));
 
       // append to all results
-      for (let i = 0; i < json.length; i++) {
+      for (let i = 0; i < json.length; i+=1) {
+        json[i]["00080060"] = { Value: [modality], vr: "CS" };
+
+        json[i]["00280002"] = { Value: [samplesPerPixel], vr: "US" };
+        json[i]["00280004"] = { Value: [photometricInterpretation], vr: "CS" };
+        json[i]["00280010"] = { Value: [rows], vr: "US" };
+        json[i]["00280011"] = { Value: [cols], vr: "US" };
+        json[i]["00280030"] = { Value: [px, py], vr: "DS" };
         json[i]["00280100"] = { Value: [bitsAllocated], vr: "US" };
         json[i]["00280101"] = { Value: [bitsStored], vr: "US" };
         json[i]["00280102"] = { Value: [highBit], vr: "US" };
-        json[i]["00280010"] = { Value: [rows], vr: "US" };
-        json[i]["00280011"] = { Value: [cols], vr: "US" };
-        json[i]["00280030"] = { Value: [pixelSpacing], vr: "DS" };
-        json[i]["00080060"] = { Value: [modality], vr: "CS" };
+        json[i]["00280103"] = { Value: [pixelRepresentation], vr: "US" };
+        json[i]["00281050"] = { Value: [wc], vr: "DS" };
+        json[i]["00281051"] = { Value: [ww], vr: "DS" };
+        json[i]["00281052"] = { Value: [rescaleIntercept], vr: "DS" };
+        json[i]["00281053"] = { Value: [rescaleSlope], vr: "DS" };
       }
       res.json(json);
     });
@@ -289,9 +308,10 @@ app.get(
     logger.info(studyInstanceUid, seriesInstanceUid, sopInstanceUid, frame);
 
     const storagePath = config.get("storagePath");
-    const pathname = path.join(storagePath, studyInstanceUid, sopInstanceUid) + ".dcm";
+    const pathname = path.join(storagePath, studyInstanceUid, `${sopInstanceUid}.dcm`);
 
-    fs.exists(pathname, function(exist) {
+
+    fs.exists(pathname, exist => {
       if (!exist) {
         // if the file is not found, return 404
         res.statusCode = 404;
@@ -300,11 +320,15 @@ app.get(
       }
 
       // read file from file system
-      fs.readFile(pathname, function(err, data) {
+      fs.readFile(pathname, (err, data) => {
         if (err) {
           res.statusCode = 500;
           res.end(`Error getting the file: ${err}.`);
         } else {
+          const dataset = dicomParser.parseDicom(data);
+          const pixelDataElement = dataset.elements.x7fe00010;
+          const buffer = Buffer.from(dataset.byteArray.buffer, pixelDataElement.dataOffset, pixelDataElement.length);
+
           const term = "\r\n";
           const boundary = crypto.randomBytes(16).toString("hex");
           const contentId = crypto.randomBytes(16).toString("hex");
@@ -319,7 +343,7 @@ app.get(
           res.write(`Content-ID:${contentId}${term}`);
           res.write(`Content-Type:application/octet-stream${term}`);
           res.write(term);
-          res.write(data);
+          res.write(buffer);
           res.write(endline);
           res.end();
         }
